15edc8c8921ac547426a2b106e483c1354fab6d1
itsarjunfr <itsarjunfr@gmail.com>
2026-01-23 12:01:48 +0800

    initial commit

--- ageclass.py
+++ ageclass.py
@@ -1,5 +1,7 @@
 age = int(input('Enter your age: '))
 if age>=10 and age<=20:
     print('You are permitted to join the class.')
+elif age<0:
+    print('Enter a valid age.')
 else:
     print('You are not allowed to join the class.')
